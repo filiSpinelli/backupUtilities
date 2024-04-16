7e92225863c14eec358e356e1b50d51238b903ee
filiSpinelli <filippo.spinelli96@gmail.com>
2024-04-16 13:54:05 +0200

    Update MainWindow
    - Add Comments
    - English translation of the UI

--- mainwindow.py
+++ mainwindow.py
@@ -8,27 +8,31 @@ import os
 import pandas as pd
 import shutil
 from datetime import datetime, timedelta
-
+ 
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         '''
-            Initialization of the Main Window
+        Initialization of the Main Window
         '''
         super().__init__()
         
-        #initialize the QSettings object
+        # Initialize the QSettings object
         self.settings = QSettings("Demos", "BackupFileLog")
+        
+        # Check if the fileToBackup setting exists, otherwise set it to an empty string
         if self.settings.contains("fileToBackup"):
             self.fileToBackup = self.settings.value("fileToBackup")
         else: 
             self.fileToBackup = ''
 
+        # Check if the backupPath setting exists, otherwise set it to the current working directory
         if self.settings.contains("backupPath"):
             self.backupPath = self.settings.value("backupPath")
         else:
             self.backupPath = os.getcwd()
         
+        # Check if the backupTime setting exists, otherwise set it to 1 minute
         if self.settings.contains("backupTime"):
             self.backupTime = self.settings.value("backupTime")
         else:
@@ -36,34 +40,33 @@ class MainWindow(QMainWindow):
 
         print(self.fileToBackup)
 
-        self.setWindowTitle("Backup File Log")
+        self.setWindowTitle("File Backup")
         self.setFixedSize(QSize(700,300))
 
-        # widgets
+        # Widgets
         self.titleLab = QLabel("<b>Backup file</b>")
         self.statusLab = QLabel()
         self.statusLab.setFixedSize(500,30)
-        self.rightsLab = QLabel("Copyright© 2024 Demos S.R.L. Tutti i diritti riservati.")
-        self.backupBtn = QPushButton("Esegui backup")
+        self.rightsLab = QLabel("Copyright© 2024 Filippo Spinelli. All rights reserved.")
+        self.backupBtn = QPushButton("Run backup")
         self.backupBtn.setFixedSize(QSize(170,30))
         self.backupBtn.clicked.connect(self.onBackupBtnClicked)
-        self.continuosBackupBtn = QPushButton("Esegui backup continuo")
+        self.continuosBackupBtn = QPushButton("Run Continuous Backup")
         self.continuosBackupBtn.setFixedSize(QSize(170,30))
         self.continuosBackupBtn.clicked.connect(self.onContinuosBackupBtnClicked)
-        self.searchFolderBtn = QPushButton("Seleziona la cartella")
+        self.searchFolderBtn = QPushButton("Select Folder")
         self.searchFolderBtn.setFixedSize(QSize(170,30))
         self.searchFolderBtn.clicked.connect(self.onSearchFolderBtnClicked)
-        self.searchFilesBtn = QPushButton("Seleziona file")
+        self.searchFilesBtn = QPushButton("Select File")
         self.searchFilesBtn.setFixedSize(QSize(170,30))
         self.searchFilesBtn.clicked.connect(self.onSearchFilesBtnClicked)
-        #Selettore del tempo di backup continuo
+        # Selettore del tempo di backup continuo
         self.backupTimeSelector = QSpinBox()
         self.backupTimeSelector.setRange(1, 60)
         self.backupTimeSelector.setValue(self.backupTime)
-        self.backupTimeSelector.setSuffix(" minuti")
+        self.backupTimeSelector.setSuffix(" min")
         self.backupTimeSelector.setFixedSize(QSize(100, 30))
        
-
         self.folderPathLineEdit = QLineEdit()
         self.folderPathLineEdit.setFixedSize(QSize(350,30))
         self.folderPathLineEdit.setText(self.backupPath)
@@ -72,11 +75,11 @@ class MainWindow(QMainWindow):
         if self.fileToBackup != '':
             self.fileNameLab.setText(self.fileToBackup)
         else:
-            self.fileNameLab.setText("Nessun File selezionato")
+            self.fileNameLab.setText("No File Selected")
 
         self.statusBar = QStatusBar()
         self.setStatusBar(self.statusBar)
-        self.statusBar.showMessage("Pronto", 1000)
+        self.statusBar.showMessage("Ready", 1000)
 
         self.wrapperLayout = QVBoxLayout()
 
@@ -96,63 +99,79 @@ class MainWindow(QMainWindow):
         self.wrapperLayout.addWidget(self.statusLab)
         self.wrapperLayout.addWidget(self.rightsLab)
 
-
         widget = QWidget()
         widget.setLayout(self.wrapperLayout)
         self.setCentralWidget(widget)
 
         self.isContinuosBackupActive = False
-
+        # comment if you want to disbale the automatic backup at the startup
         if self.fileToBackup != '':
             self.onContinuosBackupBtnClicked()
             
 
     def onSearchFolderBtnClicked(self):
+        '''
+        Event handler for the "Seleziona la cartella" button click.
+        Opens a file dialog to select a backup folder and updates the folderPathLineEdit.
+        '''
         dialog = QFileDialog()
-        self.backupPath = dialog.getExistingDirectory(None, "Seleziona la cartella",dir=self.backupPath)
+        self.backupPath = dialog.getExistingDirectory(None, "Select Folder",dir=self.backupPath)
         self.folderPathLineEdit.setText(self.backupPath)
 
     def onSearchFilesBtnClicked(self):
+        '''
+        Event handler for the "Seleziona file" button click.
+        Opens a file dialog to select files for backup and updates the fileNameLab.
+        '''
         dialog = QFileDialog()
-        files = dialog.getOpenFileNames(None, "Seleziona i file da backup")
-        #il nome del file lo scrivo in fileNameLab
+        files = dialog.getOpenFileNames(None, "Select file to backup")
+        
+        # Update the fileNameLab with the selected file
         if len(files[0]) > 1:
-            self.fileNameLab.setText("Troppi file selezionati")
+            self.fileNameLab.setText("More than one file selected")
         elif len(files[0]) == 0:
-            self.fileNameLab.setText("Nessun file selezionato")
+            self.fileNameLab.setText("No File selected")
         else:
             self.fileNameLab.setText(files[0][0])
             self.fileToBackup = files[0][0]
             
     def onContinuosBackupBtnClicked(self):
-        
+        '''
+        Event handler for the "Esegui backup continuo" button click.
+        Starts or stops continuous backup based on the current state of isContinuosBackupActive.
+        '''
         self.backupTime = self.backupTimeSelector.value()
         self.settings.setValue("backupTime",  self.backupTime)
         self.settings.sync()
 
         if self.isContinuosBackupActive:
             self.isContinuosBackupActive = False
-            self.continuosBackupBtn.setText("Esegui backup continuo")
+            self.continuosBackupBtn.setText("Run Continuous Backup")
             self.timer.stop()
             self.backupBtn.setEnabled(True)
             self.searchFilesBtn.setEnabled(True)
             self.searchFolderBtn.setEnabled(True)
             self.backupTimeSelector.setEnabled(True)
+            self.folderPathLineEdit.setEnabled(True)
         else:
             self.isContinuosBackupActive = True
-            #Cambia il nome della label in Stop backup
-            self.continuosBackupBtn.setText("Stop backup")
+            # Change the label text to "Stop backup"
+            self.continuosBackupBtn.setText("Stop Backup")
             self.backupBtn.setEnabled(False)
             self.searchFilesBtn.setEnabled(False)
             self.searchFolderBtn.setEnabled(False)
             self.backupTimeSelector.setEnabled(False)
+            self.folderPathLineEdit.setEnabled(False)
             self.onBackupBtnClicked()
             self.timer = QTimer()
             self.timer.timeout.connect(self.onBackupBtnClicked)
             self.timer.start(self.backupTimeSelector.value()*60000)
 
     def onBackupBtnClicked(self):
-        
+        '''
+        Event handler for the "Esegui backup" button click and continuous backup timer.
+        Performs the backup operation and updates the status bar.
+        '''
         self.settings.setValue("fileToBackup", self.fileToBackup)
         self.settings.setValue("backupPath", self.backupPath)
         self.settings.sync()
@@ -162,24 +181,22 @@ class MainWindow(QMainWindow):
         newFilesPath = []
         
         if self.fileToBackup != '':
-
             fileName = os.path.basename(self.fileToBackup).split('\\')[-1]
-
             destination_path = self.backupPath + "/" + fileName
 
             shutil.copy(self.fileToBackup, destination_path)
 
             if os.path.exists(destination_path):
-                print("Backup del file " + fileName +" nella cartella: " + destination_path)
+                print("Backup file " + fileName +" into folder: " + destination_path)
                 newFilesPath.append(destination_path)
             
             if len(newFilesPath) > 0:
-                statusText = "Ultimo backup : "+ current_date
+                statusText = "Last backup : "+ current_date
                 self.statusBar.showMessage(statusText, self.backupTime*60000/2)
 
         else:
-            print("Nessun file selezionato")
-            self.statusBar.showMessage("Nessun file selezionato", 10000)
+            print("No file selected")
+            self.statusBar.showMessage("No file selected", 10000)
 
                   
       
